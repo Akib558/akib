f9ccdb37e7d251c2015ae9b1e7456e4d90ff00c5
akib <kryptonianargon@gmail.com>
2025-07-26 13:53:23 +0600

    imported to angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Akib';
+  email = "saidulislamakib99@gmail.com"
+
+  openPDF() {
+    window.open('assets/Md. Saidul Islam Akib.pdf', '_blank');
+  }
+
+  openLink(url: string) {
+    window.open(url, '_blank');
+  }
+
+  scrollTo(section: string) {
+    document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  toggleMenu() {
+    // Add your menu toggle logic if you use a hamburger nav
+  }
+
 }
